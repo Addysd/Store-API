b5ea872191342ffcdbd82b97b3465862d87364e5
Adel <adeldsouza8@gmail.com>
2024-12-14 00:32:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+require('express-async-errors')
+const connectDB=require('./db/connect')
+
+const express= require('express')
+const app=express()
+
+const productsRouter=require('./routes/products')
+
+const notFoundMiddleware=require('./middleware/not-found')
+const errorMiddleware=require('./middleware/error-handler')
+
+//middlware
+app.use(express.json())
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('<h1>Store API</h1><a href="/api/v1/products"> products route</a>')
+})
+app.use('/api/v1/products',productsRouter)
+//products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port= process.env.PORT||3000
+
+const start=async()=>{
+    try{
+        //connectdb
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}`))
+    }catch(error){
+        console.log(error)
+    }
+}
+start()
